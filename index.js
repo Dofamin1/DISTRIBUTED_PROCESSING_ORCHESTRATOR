26f8d9b625bb533b5634f6618ad0169d9608cc35
Anton Kovalenko <user@Admins-MacBook-Pro.local>
2020-02-27 18:10:20 +0200

    add newFeatures - check if nodes is alive + saving their uuid

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const cote = require("cote");
+const { log } = require("./helpers")
 const exampleTask = {
     command: 'do something'
 }
@@ -13,26 +14,52 @@ class EventController {
     }
 
     publishEvent({ eventName, val}) {
+        log(`EVENT ${eventName} PUBLISHED`)
 
-        console.log('publish event')
         this.publisher.publish(eventName, val);
+    }
+
+    sendEvent({ type, value }, callback) {
+        log(`EVENT ${type} SENDED`)
+
+        this.requester.send({ type, value }, response => {
+          callback(response);
+        });
     }
 }
 
 class Orchestrator {
     constructor() {
         this.eventController = new EventController();
+        this.connectedWorkers = [];
+        this.masterUuid;
+        this._startTochekIfAliveWorkers();
     }
 
     sendTask() {
         this.eventController.publishEvent({ eventName: 'task', val: JSON.stringify(exampleTask) })
     }
 
-    strartTaskSending() {
+    startSendingTasks() {
         setInterval(this.sendTask.bind(this), 10000)
     }
+
+    _startTochekIfAliveWorkers() {
+        const checkIfAlive = () => this.eventController.sendEvent({ type: 'checkIfAlive' }, res => {
+            res.role == "master" ? this._saveMasterUuid(res.uuid) : this._saveWorkerUuid(res.uuid);
+        });
+        setInterval(checkIfAlive, 1000)
+    }
+
+    _saveWorkerUuid(uuid){
+        this.connectedWorkers.push(uuid);
+    }
+    _saveMasterUuid(uuid) {
+        this.masterUuid = uuid;
+    }
+    
 }
 
 const orchestrator = new Orchestrator();
 
-orchestrator.strartTaskSending();
+orchestrator.startSendingTasks();

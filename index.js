462b8a8c09f811da2de7e78c4d31752e8fe4a10e
Anton_Kovalenko <gugenot990@gmail.com>
2020-03-08 23:11:28 +0200

    prettier commit

--- index.js
+++ index.js
@@ -1,103 +1,116 @@
-const {log, levels} = require("./helpers");
-const EventController = require('./eventController');
-const dockerRunnerInitializer = require('./node/DockerNodeRunner');
+const { log, levels } = require("./helpers");
+const EventController = require("./eventController");
+const dockerRunnerInitializer = require("./node/DockerNodeRunner");
 // noinspection SpellCheckingInspection
-const uuidv4 = require('uuid/v4');
+const uuidv4 = require("uuid/v4");
 
 const DEFAULT_TIMEOUT = 10000;
 
 class Orchestrator {
-    constructor(nodeRunner, timeout = DEFAULT_TIMEOUT) {
-        this.eventController = new EventController();
-        this.observedNodes = [];
-        this.nodeRunner = nodeRunner;
-        this.timeout = timeout;
-        this.statusLast = new Map();
-    }
+  constructor(nodeRunner, timeout = DEFAULT_TIMEOUT) {
+    this.eventController = new EventController();
+    this.observedNodes = [];
+    this.nodeRunner = nodeRunner;
+    this.timeout = timeout;
+    this.statusLast = new Map();
+  }
 
-    listenNodes() {
-        setInterval(async () => {
-            log("Status is being checked");
-            this.eventController.publishEvent({
-                eventName: 'nodes_list',
-                val: Array.from(this.statusLast.keys())
-            });
-            await this._runFailedNodes();
-            this.statusLast.clear();
-            this.eventController.sendEvent({type: 'status'}, res => {
-                log(`I have got result`, levels.DEBUG);
-                this.statusLast.set(res.uuid, res.role);
-            });
-        }, this.timeout);
-    }
+  listenNodes() {
+    setInterval(async () => {
+      log("Status is being checked");
+      this.eventController.publishEvent({
+        eventName: "nodes_list",
+        val: Array.from(this.statusLast.keys())
+      });
+      await this._runFailedNodes();
+      this.statusLast.clear();
+      this.eventController.sendEvent({ type: "status" }, res => {
+        log(`I have got result`, levels.DEBUG);
+        this.statusLast.set(res.uuid, res.role);
+      });
+    }, this.timeout);
+  }
 
-    runNodes() {
-        return Promise.all(
-            this.observedNodes.map(node => this._runNode(node))
+  runNodes() {
+    return Promise.all(
+      this.observedNodes.map(node => this._runNode(node))
+    ).then(() => log(`Nodes are run`));
+  }
+
+  withNodes(nodes) {
+    this.observedNodes.push(...nodes);
+    nodes.forEach(node => this.statusLast.set(node.uuid, node.role));
+    log(
+      `Nodes {alive: ${this.statusLast.size}, total: ${
+        this.observedNodes.length
+      }}`
+    );
+    return this;
+  }
+
+  shutDown() {
+    log("init shutdown");
+    return Promise.all(
+      this.observedNodes.map(node =>
+        this.nodeRunner
+          .stop({ containerId: node.uuid })
+          .then(() => this.nodeRunner.remove({ containerId: node.uuid }))
+      )
+    )
+    .then(() => log("Instances are turned of and removed"));
+  }
+
+  _runNode(node) {
+    return this.nodeRunner.run({
+      host: node.host,
+      port: node.port,
+      args: `FIRST_START_NODE_STATUS=${node.isMaster} UUID=${node.uuid}`,
+      containerId: node.uuid
+    });
+  }
+
+  _cleanUpNode(node) {
+    return this.nodeRunner.remove({ containerId: node.uuid });
+  }
+
+  _runFailedNodes() {
+    return new Promise(resolve => {
+      log(
+        `Nodes {alive: ${this.statusLast.size}, total: ${
+          this.observedNodes.length
+        }}`
+      );
+      if (this.statusLast.size !== this.observedNodes.length) {
+        const failedNodes = this.observedNodes.filter(
+          node => !this.statusLast.get(node.uuid)
+        );
+        Promise.all(
+          failedNodes.map(node =>
+            this._cleanUpNode(node).then(() => this._runNode(node))
+          )
         )
-        .then(() => log(`Nodes are run`));
-    }
-
-    withNodes(nodes) {
-        this.observedNodes.push(...nodes);
-        nodes.forEach(node => this.statusLast.set(node.uuid, node.role));
-        log(`Nodes {alive: ${this.statusLast.size}, total: ${this.observedNodes.length}}`);
-        return this;
-    }
-
-    shutDown() {
-        log('init shutdown');
-        return Promise.all(
-            this.observedNodes.map(node => this.nodeRunner.stop({containerId: node.uuid})
-                .then(() => this.nodeRunner.remove({containerId: node.uuid})))
-        )
-        .then(() => log('Instances are turned of and removed'));
-    }
-
-    _runNode(node) {
-        return this.nodeRunner.run({
-            host: node.host,
-            port: node.port,
-            args: `FIRST_START_NODE_STATUS=${node.isMaster} UUID=${node.uuid}`,
-            containerId: node.uuid
-        });
-    }
-
-    _cleanUpNode(node) {
-        return this.nodeRunner.remove({containerId: node.uuid});
-    }
-
-    _runFailedNodes() {
-        return new Promise(resolve => {
-            log(`Nodes {alive: ${this.statusLast.size}, total: ${this.observedNodes.length}}`);
-            if (this.statusLast.size !== this.observedNodes.length) {
-                const failedNodes = this.observedNodes.filter(node => !this.statusLast.get(node.uuid));
-                Promise.all(failedNodes.map(node =>
-                    this._cleanUpNode(node).then(() => this._runNode(node))
-                    )
-                )
-                .then(() => resolve(failedNodes.length))
-                .then(() => log(`Failed nodes is recovered: ${failedNodes}`));
-            } else {
-                resolve(0);
-            }
-        });
-    }
+          .then(() => resolve(failedNodes.length))
+          .then(() => log(`Failed nodes is recovered: ${failedNodes}`));
+      } else {
+        resolve(0);
+      }
+    });
+  }
 }
 
-const NODE = 'node';
-const MASTER = 'master';
+const NODE = "node";
+const MASTER = "master";
 
 dockerRunnerInitializer("nodes")
-    .then(nodeRunner => new Orchestrator(nodeRunner)
-        .withNodes([
-            {host: 'localhost', port: 7777, uuid: uuidv4(), role: MASTER},
-            {host: 'localhost', port: 8888, uuid: uuidv4(), role: NODE}
-        ])
-    )
-    .then(orchestrator => orchestrator.runNodes()
-        .then(() => orchestrator.listenNodes())
-        .then(() => setTimeout(orchestrator.shutDown, 90000))
-    );
-
-
+  .then(nodeRunner =>
+    new Orchestrator(nodeRunner).withNodes([
+      { host: "localhost", port: 7777, uuid: uuidv4(), role: MASTER },
+      { host: "localhost", port: 8888, uuid: uuidv4(), role: NODE }
+    ])
+  )
+  .then(orchestrator =>
+    orchestrator
+      .runNodes()
+      .then(() => orchestrator.listenNodes())
+      .then(() => setTimeout(orchestrator.shutDown, 90000))
+  );
